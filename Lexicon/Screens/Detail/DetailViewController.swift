9132f03b4d94526fe04ea3b8796612b37fd219ff
Andrew Hershberger <andrew.d.hershberger@gmail.com>
2017-09-28 17:49:30 -0400

    Add empty detail screen

---
+++ Lexicon/Screens/Detail/DetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewController.swift
+//  Lexicon
+//
+//  Created by Andrew Hershberger on 9/28/17.
+//  Copyright © 2017 Andrew Hershberger. All rights reserved.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+}
